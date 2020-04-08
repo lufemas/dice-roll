da2d9680a40780ff00cdf7e6d85729b9335dc15d
JR Maschietto (lufemas) <maschietto.jr@gmail.com>
2020-04-08 17:15:00 -0400

    Dice Roll implementation #1 - Comments

--- js/main.js
+++ js/main.js
@@ -11,12 +11,20 @@ const $ = document;
 const $rollBtn = $.getElementById(`roll-btn`);
 const $diceFaces = $.getElementsByClassName(`dice-face`);
 const $scoreNumber = $.getElementById(`score-number`);
-console.log($diceFaces)
 
+// - When the user clicks on the ROLL button it'll assign a random value between 1 and 6
 $rollBtn.addEventListener(`click`, function(e) {
+
+    // - Store a variable with the rolled number, let's call
     let roll = Math.floor(Math.random() * 6) + 1
+
+    // - Change the number rolled displayed on the browser
     $scoreNumber.innerText = roll;
+
+    // Debugging
     console.log(roll);  // 1, 2, 3, 4, 5, or 6
+
+    // - Depending on the number we rolled the briwser will show the correct image
     for( const face of $diceFaces ){
         if(face.id === `face-${roll}`) {
             face.classList.add(`show-face`)
